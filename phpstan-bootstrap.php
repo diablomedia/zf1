565b7d4559613e1563302ea3f3667a9c16c5eddd
Jay Klehr <jay@diablomedia.com>
2020-01-08 11:10:01 -0700

    Some composer updates for php 7.4 tests to pass

--- phpstan-bootstrap.php
+++ phpstan-bootstrap.php
@@ -1,5 +1,4 @@
 <?php
-
 // The Zend_Service_WindowsAzure_CommandLine classes try to bootstrap the Zend_Service_Console_Command
 // component when the classes are loaded (bottom of file), which phpstan will trigger when it
 // autoloads the classes.  This prevents it from bootstrapping (which prevents some exceptions being thrown)
